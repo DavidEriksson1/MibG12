708c7fb4328950053af1ce752a2ae40bc0448cd2
eriknilsson98 <96289465+eriknilsson98@users.noreply.github.com>
2021-12-22 13:35:18 +0100

    Update HuvudMenyUtomjording.java

--- src/MibG12/HuvudMenyUtomjording.java
+++ src/MibG12/HuvudMenyUtomjording.java
@@ -107,9 +107,10 @@ public class HuvudMenyUtomjording extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-         
-         
+        InloggningUtomjording iU = new InloggningUtomjording(idb);
+        iU.setVisible(true);
+        dispose();
+
     }//GEN-LAST:event_jButton1ActionPerformed
     public void setValkommenUtomjording()
     {
