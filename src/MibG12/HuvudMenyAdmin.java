00e9a9af12d17b7f5b741b24064801ae938a9f62
DavidEriksson1 <erikssondavid97@gmail.com>
2022-01-06 10:11:19 +0100

    pilla runt

--- src/MibG12/HuvudMenyAdmin.java
+++ src/MibG12/HuvudMenyAdmin.java
@@ -137,8 +137,11 @@ public class HuvudMenyAdmin extends javax.swing.JFrame {
  // Combobox för att visa alternativ för hantering gällande agenter
         int i = jComboAgent.getSelectedIndex();
         
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 <<<<<<< HEAD
+=======
+>>>>>>> parent of 2082af4 (jj)
         if(i == 4){
             new AdminTaBortAgent(idb, agent).setVisible(true);
             dispose();
@@ -150,6 +153,7 @@ public class HuvudMenyAdmin extends javax.swing.JFrame {
             dispose();
         }
         
+<<<<<<< HEAD
 =======
 >>>>>>> parent of edecc00 (Skapa jFrame ge adminstatus)
 =======
@@ -170,6 +174,8 @@ public class HuvudMenyAdmin extends javax.swing.JFrame {
                 }
         
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of 2082af4 (jj)
         if(i == 5){
             AdminOmrådesChef adminOmrådesChef = new AdminOmrådesChef(idb, agent);
             adminOmrådesChef.setVisible(true);
